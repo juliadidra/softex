99f3408ffce94cc3d2cb69304e44890cb00daeeb
didira <71778816+juliadidra@users.noreply.github.com>
2022-09-09 00:47:35 -0300

    Update db.js

--- banco/db.js
+++ banco/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize ('sys', 'root', 'Juliadi12345677', {
+const sequelize = new Sequelize ('sys', 'root', '1234', {
     dialect: 'mysql',
     host: 'localhost'
 })
