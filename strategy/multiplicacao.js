112e971e4ccaab302715b59adf0ce4f25d2dd305
didira <71778816+juliadidra@users.noreply.github.com>
2022-10-26 09:36:39 -0300

    Create multiplicacao.js

---
+++ strategy/multiplicacao.js
@@ -0,0 +1,9 @@
+class multiplicacao {
+    constructor(){
+        this.calculate = (num1, num2) =>
+        {
+            return num1  * num2;
+        }
+    }
+}
+module.exports = multiplicacao;
